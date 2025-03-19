2c89b8ce211b6303eed2036f0c63399d635c9777
tetron225 <skim6502@gmail.com>
2025-03-19 07:12:07 -0400

    added images for weather images. will test dynamic import

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ import { gettingAPI } from "./weatherfunction.js";
 
 const pressButton = document.querySelector('#press');
 const anInput = document.querySelector('#city');
+let container = document.querySelector('#container');
+
+
 
 pressButton.addEventListener('click', () => {
+    while(container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
+    }
+
     console.log(anInput.value);
     gettingAPI(anInput.value);
 });
